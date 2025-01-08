0a60abf48c62f09b6fc53da025aaa5b88060276d
Tj Hall <tjhall047@gmail.com>
2025-01-08 00:53:19 -0500

    Added security headers with helmet and logging with pino for 404 or errors. Lastly, added a global 404 to tell the user its just a static site bro chill

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const helmet = require('helmet');
+const logger = require('pino');
 
 const PORT = process.env.PORT || 8000;
+
+app.use(helmet());
+app.disable('x-powered-by');
 
 app.use(express.static(path.join(__dirname, "/public")));
 
@@ -10,7 +15,14 @@ app.get('/', (req, res) => {
   res.sendFile( __dirname + '/public/index.html');
 });
 
+app.use((req, res, next) => {
+  res.status(400).send("Sorry can't find that! Stop looking so hard . . .");
+});
 
+app.use((err, req, res, next) => {
+  logger.error(err.stack());
+  res.status(500).send('Something broke! STOP! WHATEVER! YOUR! DOING! please . . . ;?');
+});
 
 app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
 /**
