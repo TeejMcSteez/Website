9fb62af21be196aa6e5f888ac595994375825ae0
TeejMcSteez <tjhall047@gmail.com>
2025-01-08 20:40:26 -0500

    Called a stack function not the object of stack from err

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use((req, res, next) => {
 });
 
 app.use((err, req, res, next) => {
-  logger.error(err.stack());
+  logger.error(err.stack);
   res.status(500).send('Something broke! STOP! WHATEVER! YOUR! DOING! please . . . ;?');
 });
 
