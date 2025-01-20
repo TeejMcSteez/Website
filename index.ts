0d66d30f1de0cba7c6326f9654972b84ebfbf1d6
TeejMcSteez <tjhall047@gmail.com>
2025-01-20 17:04:04 -0500

    Want to add of file share server for stuff that isnt to sensitive we shall see what I can do

--- index.ts
+++ index.ts
@@ -5,6 +5,9 @@ const path = require('path');
 const app = express();
 const helmet = require('helmet'); // DOC: https://helmetjs.github.io/
 const logger = require('pino').pino(); // REPO: https://github.com/pinojs/pino
+const multer = require('multer');
+
+const upload = multer({ dest: 'uploads/' });
 
 const PORT = process.env.PORT || 8000;
 
@@ -18,22 +21,29 @@ app.get('/', (req: Request, res: Response) => {
   res.sendFile( __dirname + '/public/index.html');
 });
 
+app.get('/share', (req: Request, res: Response) => {
+  res.sendFile( __dirname + '/public/share.html');
+});
+
+app.post('/upload', upload.single('file'), (req: Request, res: Response) => {
+  if (!req.file) {
+    return res.status(400).send('No files were uploaded.');
+  }
+  res.json({ message: 'File uploaded successfully', file: req.file });
+});
+
 app.use((req: Request & { secure: boolean }, res: Response, next: NextFunction) => {
   if (req.secure) {
     next();
   } else {
-    res.redirect('https://'+req.host+req.url);
+    res.redirect('https://'+req.hostname+req.url);
   }
   res.status(400).send("Sorry can't find that! Stop looking so hard . . .");
 });
 
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-  logger.error(err.stack);
+  logger.error(err);
   res.status(500).send('Something broke! STOP! WHATEVER! YOUR! DOING! please . . . ;?');
 });
 
 app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
-/**
- * Docs: 
- * // To start tailwind run npx tailwindcss initailwindcss -i ./public/stylesheet.css -o ./public/tailwind.css --watch
- */
---
+++ public/upload.js
@@ -0,0 +1,25 @@
+document.getElementById('upload').addEventListener('submit', function(e) {
+    e.preventDefault();
+    
+    const fileIn = document.getElementById('file');
+    const file = fileIn.files[0];
+
+    if (file) {
+        const formData = new FormData();
+        formData.append('file', file);
+
+        fetch('/upload', {
+            method: 'POST',
+            body: formData
+        })
+        .then(response => response.json())
+        .then( data => {
+            console.log("success");
+        })
+        .catch((error) => {
+            console.error('Error: ' + error);
+        });
+    } else {
+        alert('No file selected');
+    }
+});
